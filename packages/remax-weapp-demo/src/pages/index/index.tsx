40cd4d90092856179d3fff0d12e1bba9424cfd63
ben <544652471@qq.com>
2020-05-27 12:11:44 +0800

    feat: add publishconfig / access:public.

--- packages/remax-weapp-demo/src/pages/index/index.tsx
+++ packages/remax-weapp-demo/src/pages/index/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { EggButton } from '@redchili/egg-ui/remax';
 import { View } from 'remax/one';
 
-export default () => {
+export default () => {cd 
   return (
     <View >
       <EggButton >DEFAULT</EggButton>
