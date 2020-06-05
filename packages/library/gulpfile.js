1c8fa16c84aa3e63244732b5ff7955a9a0b048fe
ben <544652471@qq.com>
2020-06-05 17:12:26 +0800

    feat: add some feature.

--- packages/library/gulpfile.js
+++ packages/library/gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require("gulp");
 var postcss = require("gulp-postcss");
+var postcssCssVariables = require("postcss-css-variables");
+var postcssApply = require('postcss-apply');
+// var postcssPresetEnv = require("postcss-preset-env");
+var postcssImport = require("postcss-import");
 var sourcemaps = require("gulp-sourcemaps");
+var cssnano = require("cssnano");
 
 function defaultTask(cb) {
   gulp
     .src("src/**/*.css")
     .pipe(sourcemaps.init())
-    .pipe(postcss())
+    .pipe(
+      postcss([
+        postcssImport("src/style/vars.css"),
+        postcssApply(),
+        postcssCssVariables(),
+        cssnano(),
+      ])
+    )
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist/"));
+    .pipe(gulp.dest('dist/'));
   // place code for your default task here
   cb();
 }
--- packages/weapp-demo/src/pages/form/index.tsx
+++ packages/weapp-demo/src/pages/form/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { EggForm, EggFieldItem, EggSubmit } from '@redchili/egg-ui/src';
+import { EggForm, EggFieldItem, EggSubmit } from '@redchili/egg-ui';
 import { Input, Label } from 'remax/one';
 
 export default function FormDemo() {
@@ -13,11 +13,15 @@ export default function FormDemo() {
 
     return (
         <EggForm ref={formRef} onSubmit={(data) => { console.log(data) }}>
-            <EggFieldItem rule={[{ required: true, message: "请输入值" }]}>
+            <EggFieldItem name="nickname" rule={[{ required: true, message: "请输入值" }]}>
                 <Label htmlFor="nickname" >测试</Label>
                 <Input name="nickname" />
             </EggFieldItem>
             <EggSubmit >提交</EggSubmit>
         </EggForm>
     )
+}
+
+function EggInput () {
+    return <Input />
 }
