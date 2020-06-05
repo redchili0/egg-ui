a2588b1a136aaf72941db6169fa935d32c6b110c
ben <544652471@qq.com>
2020-06-05 16:41:23 +0800

    feat: add some feature.

--- packages/library/gulpfile.js
+++ packages/library/gulpfile.js
@@ -1,16 +1,35 @@
 var gulp = require("gulp");
 var postcss = require("gulp-postcss");
+var postcssCssVariables = require("postcss-css-variables");
+var postcssImport = require("postcss-import");
 var sourcemaps = require("gulp-sourcemaps");
+var cssnano = require("cssnano");
 
-function defaultTask(cb) {
+function defaultTask(cb, dest) {
   gulp
     .src("src/**/*.css")
     .pipe(sourcemaps.init())
-    .pipe(postcss())
+    .pipe(
+      postcss([
+        postcssImport("src/style/vars.css"),
+        postcssCssVariables(),
+        cssnano(),
+      ])
+    )
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist/"));
+    .pipe(gulp.dest(dest));
   // place code for your default task here
   cb();
 }
 
-exports.default = defaultTask;
+exports.dev = function dev(cb) {
+  gulp.watch("src/**/*.css", function () {
+    defaultTask(cb, "src/");
+  });
+  cb();
+};
+
+exports.prod = function prod(cb) {
+  defaultTask(cd, "dist/");
+  cb();
+};
